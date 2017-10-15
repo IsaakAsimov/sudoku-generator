b046d06e41d2b09550a2ace47233aa35ad24a710
Average-user <lucaspolymeris@gmail.com>
2017-10-15 15:07:39 -0300

    Improvement in the algorithm

--- SudokuGenerator.hs
+++ SudokuGenerator.hs
@@ -30,27 +30,29 @@ assoc (x:xs) n e | n == 0 = e:xs
                  | otherwise = x:assoc xs (n-1) e
 
 assocIn :: [[a]] -> (Int, Int) -> a -> [[a]]
-assocIn xs (y, x) e = assoc xs y (assoc (xs!!y) x e)
+assocIn xs (x, y) e = assoc xs x (assoc (xs!!x) y e)
 
 notRepited :: [Int] -> Bool
 notRepited = all (==1) . map length . group . sort . filter(/=0)
 
-noIlligal :: [[Int]] -> Bool
-noIlligal xs = all notRepited (columns ++ xs ++ boxes)
+noIlligal :: [[Int]] -> (Int, Int) -> Bool
+noIlligal grid (x,y) = all notRepited [column, block, grid!!x]
   where
-    columns = [[xs!!b!!a | b <- [0..8]] | a <- [0..8]]
-    boxesB = [ (0,2,0,2),(0,2,3,5),(0,2,6,8)
-             , (3,5,0,2),(3,5,3,5),(3,5,6,8)
-             , (6,8,0,2),(6,8,3,5),(6,8,6,8)]
-    boxes  = [[xs!!a!!b | a <- [x..y], b <- [u..v]] | (x,y,u,v) <- boxesB]
+    column  = [grid!!a!!y | a <- [0..8]]
+    blocksB = [ [((0,2), (0,2)), ((0,2), (3,5)), ((0,2), (6,8))]
+              , [((3,5), (0,2)), ((3,5), (3,5)), ((3,5), (6,8))]
+              , [((6,8), (0,2)), ((6,8), (3,5)), ((6,8), (6,8))]]
+    block   = [grid!!a!!b | a <- [g1..g2], b <- [h1..h2]]
+    ((g1, g2), (h1, h2)) = blocksB!!(div x 3)!!(div y 3)
 
 findEmpty :: [[Int]] -> (Int, Int)
-findEmpty xs = head [(x,y) | x <- [0..8], y <- [0..8], xs !! y !! x == 0]
+findEmpty xs = head [(x,y) | x <- [0..8], y <- [0..8], xs !! x !! y == 0]
 
-backtrack :: [[Int]] -> Int -> [Int] -> [[Int]]
-backtrack xs n l | not (noIlligal xs) = []
-                 | n == 0             = xs
-                 | otherwise          = options xs (findEmpty xs) l n l
+backtrack :: [[Int]] -> (Int, Int) -> Int -> [Int] -> [[Int]]
+backtrack xs coor n l
+                  | not (noIlligal xs coor) = []
+                  | n == 0                  = xs
+                  | otherwise               = options xs (findEmpty xs) l n l
 
 options :: [[Int]] -> (Int,Int) -> [Int] -> Int -> [Int] -> [[Int]]
 options xs (x,y) []     n l = []
@@ -58,13 +60,13 @@ options xs (x,y) (o:op) n l = if null sdk
                                  then options xs (x,y) op n l
                                  else sdk
   where
-    sdk = backtrack (assocIn xs (y,x) o) (n-1) l
+    sdk = backtrack (assocIn xs (x,y) o) (x,y) (n-1) l
 
 remove :: (RandomGen t) => Int -> [[Int]] -> t -> [[Int]]
 remove 0 xs g = xs
 remove n xs g = remove (n-1) (assocIn xs (op!!i) 0) ng
   where
-    op      = [(y,x) | x <- [0..8], y <- [0..8], xs !! y !! x /= 0]
+    op      = [(x,y) | x <- [0..8], y <- [0..8], xs !! x !! y /= 0]
     (i, ng) = randomR (0, length op - 1) g
 
 -- IO Functions ----------------------------------------------------------------
@@ -81,6 +83,6 @@ main = do
   rand <- fmap (randomR (0, 362879 :: Int)) newStdGen
   let emptyBoard =  replicate 9 (replicate 9 0)
       list       =  permutations [1..9] !! fst rand
-      sdk        =  backtrack emptyBoard 81 list
+      sdk        =  backtrack emptyBoard (0,0) 81 list
 
   prints $ remove (81 - n) sdk (snd rand)
